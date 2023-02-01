9e22fc435385e74df37cdaa1aed1cb1a5641b279
Taeil <102219209+k1k2brz@users.noreply.github.com>
2023-02-01 15:22:07 +0900

    modifty css, add img
    sticky -> fixed로 변경 사진추가

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -3,14 +3,14 @@ const Canvas = () => {
 
     return (
         <div className="cav-container">
-            <div className="cav-wrapper container__default">
-                <div className="cav-content">
-                    <p className="cav-p">안녕하세요</p>
-                    <p className="cav-bold cav-p">Front-end Developer 이태일입니다.</p>
-                    <p className="cav-p">항상 탐구하는 개발자가 되고 싶습니다.</p>
+            <div className="cav__wrapper container__default">
+                <div className="cav__content">
+                    <p className="cav__p">안녕하세요</p>
+                    <p className="cav__bold cav__p">Front-end Developer 이태일입니다.</p>
+                    <p className="cav__p">항상 탐구하는 개발자가 되고 싶습니다.</p>
                 </div>
-                <div className='cav-img-container'>
-                    <div className='cav-photo'></div>
+                <div className='cav__img-container'>
+                    <img src="/profile/profile.jpg" alt="profile" className="cav__photo" />
                 </div>
             </div>
         </div>
