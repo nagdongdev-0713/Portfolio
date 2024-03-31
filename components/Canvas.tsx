831e88d280be8e3f2268c5155c4b4c8f69f34cf4
Lee Yeon Woo <76273057+nagdongdev-0713@users.noreply.github.com>
2024-03-31 11:18:38 +0900

    Update Canvas.tsx

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -6,7 +6,7 @@ const Canvas = () => {
             <div className="cav__wrapper container__default">
                 <div className="cav__content">
                     <p className="cav__p">Hello🖐️</p>
-                    <p className="cav__bold cav__p">Im Developer!</p>
+                    <p className="cav__bold cav__p">My name is Yeon Woo LEE,</p>
                     <p className="cav__p">Attending <a href="https://school.busanedu.net/nagdong-e/">Nagdong Elementry School</a> in Busan</p>
                 </div>
                 <div className='cav__img-container'>
