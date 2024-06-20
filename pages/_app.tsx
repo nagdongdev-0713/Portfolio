65797e7f3cb033ec806b002644ea4339f2a68c21
Lee Yeon Woo <76273057+nagdongdev-0713@users.noreply.github.com>
2024-06-20 19:45:37 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,9 +13,9 @@ export default function App({ Component, pageProps }: AppProps) {
           이연우 포트폴리오
         </title>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta content="이태일 포트폴리오" />
+        <meta content="@salm0n_LEE" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:description" content="Frontend-Developer 이태일입니다." />
+        <meta property="og:description" content="낙동12 Developer" />
         <meta property="og:site_name" content="Portfolio" />
       </Head>
       {/* 과하게 움직일시 살짝 버벅거리는 현상으로 인해 보류 */}
