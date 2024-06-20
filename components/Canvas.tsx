cf748c91630ef5e22aaec7214e555e12d21b19f8
Lee Yeon Woo <76273057+nagdongdev-0713@users.noreply.github.com>
2024-06-20 19:48:42 +0900

    Update Canvas.tsx

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -5,9 +5,9 @@ const Canvas = () => {
         <div className="cav-container">
             <div className="cav__wrapper container__default">
                 <div className="cav__content">
-                    <p className="cav__p">Hello🖐️</p>
-                    <p className="cav__bold cav__p">My name is Yeon Woo LEE,</p>
-                    <p className="cav__p">Attending <a href="https://school.busanedu.net/nagdong-e/">Nagdong Elementry School</a> in Busan</p>
+                    <p className="cav__p">안녕하세요🖐️</p>
+                    <p className="cav__bold cav__p">저는 <a href="https://instagram.com/salm0n_lee">salm0n_lee</a>입니다!</p>
+                    <p className="cav__p">부산에 있는 <a href="https://school.busanedu.net/nagdong-e/">낙동초등학교</a>에 재학 중이에요</p>
                 </div>
                 <div className='cav__img-container'>
                     <img src="/profile/profile.jpg" alt="profile" className="cav__photo" />
