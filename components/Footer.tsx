e2fc2d75b1b767323a3e7324a633dbbd3c51195d
Lee Yeon Woo <76273057+nagdongdev-0713@users.noreply.github.com>
2024-07-24 09:30:20 +0900

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,13 +4,13 @@ const Footer = () => {
             <div className="footer__container">
                 <div className="footer__copyright">LEE YEONWOO</div>
                 <div className="footer__bi">
-                    <a href="https://github.com/k1k2brz" className="footer__icon"
+                    <a href="https://instagram.com/dev_salmon" className="footer__icon"
                         target="_blank" rel="noreferrer noopener">
-                        <img src="https://img.shields.io/badge/Github-000000?style=flat&logo=github&logoColor=white" alt="github" />
+                        <img src="https://img.shields.io/badge/Instagram-000000?style=flat&logo=instagram" alt="Instagram" />
                     </a>
-                    <a href="https://wonderfulwonder.tistory.com/" className="footer__icon"
+                    <a href="https://github.com/nagdongdev-0713" className="footer__icon"
                         target="_blank" rel="noreferrer noopener">
-                        <img src="https://img.shields.io/badge/Blog-000000?style=flat&logo=tistory&logoColor=white" alt="tistory"  />
+                        <img src="https://img.shields.io/badge/Github-000000?style=flat&logo=github&logoColor=white" alt="tistory"  />
                     </a>
                 </div>
             </div>
